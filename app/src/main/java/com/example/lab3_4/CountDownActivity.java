9dc259493064f021b8f1f252f662ec24e0cd16ac
Samuli <samuli.ronkko@gmail.com>
2019-01-24 15:12:30 +0200

    Valmis

--- app/src/main/java/com/example/lab3_4/CountDownActivity.java
+++ app/src/main/java/com/example/lab3_4/CountDownActivity.java
@@ -1,7 +1,14 @@
 package com.example.lab3_4;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CountDownActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class CountDownActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_down);
+
+        final TextView countDownTextView = (TextView) findViewById(R.id.countdown_text_id);
+
+        String aika = getIntent().getStringExtra("aika");
+        long kesto = Integer.parseInt(aika) * 1000;
+
+        new CountDownTimer(kesto, 1000) {
+            public void onTick(long millisUntilFinished) {
+                countDownTextView.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+            public void onFinish() {
+                countDownTextView.setText("0");
+                Uri alarm = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), alarm);
+                mediaPlayer.start();
+                new CountDownTimer(4000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+
+                    }
+
+                    public void onFinish() {
+                        mediaPlayer.stop();
+                        finish();
+                    }
+                }.start();
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/lab3_4/MainActivity.java
+++ app/src/main/java/com/example/lab3_4/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lab3_4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,5 +20,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        Intent countDownIntent = new Intent(this, CountDownActivity.class);
+        String aika = ((EditText)findViewById(R.id.edit_text_id)).getText().toString();
+        countDownIntent.putExtra("aika", aika);
+        startActivity(countDownIntent);
     }
 }
